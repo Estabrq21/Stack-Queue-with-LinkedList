97b84d77077cbd57b0922080a383bc2d5e19114f
Estabrq Belal Abdelbasset <78881087+Estabrq21@users.noreply.github.com>
2023-04-04 00:47:52 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack stack = new Stack();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+        Console.WriteLine(stack.Pop()); 
+        Console.WriteLine(stack.Pop()); 
+        Console.WriteLine(stack.Pop()); 
+       
+    }
+}
