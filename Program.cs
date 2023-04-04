97a72433c34b7efd52b78fb95ae0ddf56765d377
Estabrq Belal Abdelbasset <78881087+Estabrq21@users.noreply.github.com>
2023-04-04 12:53:55 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,26 @@
 using System;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
         Stack stack = new Stack();
+        Queue queue = new Queue();
+
         stack.Push(1);
         stack.Push(2);
         stack.Push(3);
-        Console.WriteLine(stack.Pop()); 
-        Console.WriteLine(stack.Pop()); 
-        Console.WriteLine(stack.Pop()); 
-       
+
+        Console.WriteLine(stack.Pop()); // Outputs 3
+        Console.WriteLine(stack.Pop()); // Outputs 2
+        Console.WriteLine(stack.Peek()); // Outputs 1
+
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+
+        Console.WriteLine(queue.Dequeue()); // Outputs 1
+        Console.WriteLine(queue.Dequeue()); // Outputs 2
+        Console.WriteLine(queue.Peek()); // Outputs 3
     }
 }
