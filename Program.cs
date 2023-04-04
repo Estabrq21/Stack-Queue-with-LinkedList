cf48485c7d4c3d229f78c8eba05ed9a379b7b562
Estabrq Belal Abdelbasset <78881087+Estabrq21@users.noreply.github.com>
2023-04-04 12:57:00 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,16 +11,16 @@ public class Program
         stack.Push(2);
         stack.Push(3);
 
-        Console.WriteLine(stack.Pop()); // Outputs 3
-        Console.WriteLine(stack.Pop()); // Outputs 2
-        Console.WriteLine(stack.Peek()); // Outputs 1
+        Console.WriteLine(stack.Pop()); 
+        Console.WriteLine(stack.Pop()); 
+        Console.WriteLine(stack.Peek()); 
 
         queue.Enqueue(1);
         queue.Enqueue(2);
         queue.Enqueue(3);
 
-        Console.WriteLine(queue.Dequeue()); // Outputs 1
-        Console.WriteLine(queue.Dequeue()); // Outputs 2
-        Console.WriteLine(queue.Peek()); // Outputs 3
+        Console.WriteLine(queue.Dequeue()); 
+        Console.WriteLine(queue.Dequeue()); 
+        Console.WriteLine(queue.Peek()); 
     }
 }
